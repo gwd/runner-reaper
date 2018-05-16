cbc57a103d3f7086a67ee2bbb56803c8b784b82d
George Dunlap <george.dunlap@citrix.com>
2018-05-16 16:45:27 +0100

    Add copyright notices and licensing information
    Signed-off-by: George Dunlap <george.dunlap@citrix.com>

--- runner.c
+++ runner.c
@@ -1,3 +1,24 @@
+/* 
+ * runner.c: Rogue process that's hard to kill
+ *
+ * Copyright (C) 2018 Citrix Systems UK Ltd
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 2 of the
+ * License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
